3d69dbdd15d182f39bf9747548f61fc6fbc0acf0
juliancolman <juliancolman12@gmail.com>
2024-05-31 19:19:04 -0300

    prueba

--- src/main/java/equipo1obrasocial/entities/Paciente.java
+++ src/main/java/equipo1obrasocial/entities/Paciente.java
@@ -1,5 +1,25 @@
 package equipo1obrasocial.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity 
 public class Paciente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column
+	long id;
+	
 }
